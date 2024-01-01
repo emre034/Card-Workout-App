45451d66588a8ec07bea3e7d8f88f763a14959b2
Emre Kulaber <emrekulaber34@gmail.com>
2024-01-01 19:10:54 +0300

    Add files via upload

---
+++ CWbuttons.swift
@@ -0,0 +1,42 @@
+//
+//  CWbuttons.swift
+//  CardWorkoutAppWithProgrammatic
+//
+//  Created by apple on 21.01.2023.
+//
+
+import UIKit
+
+class CWbuttons: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+        configure()
+    }
+    
+    
+    func configure() {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
